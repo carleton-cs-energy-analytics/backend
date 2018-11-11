f182abb3b17dd0479f26d590c0b3e44fbbff46de
Eva Grench <grenche@carleton.edu>
2018-11-11 17:57:25 -0600

    Fixed removing added points and added a test for adding multiple values at a time.

--- tests/ValueTests.py
+++ tests/ValueTests.py
@@ -5,11 +5,17 @@ from backend.database.models import Values, execute_and_commit
 class ValueTests(unittest.TestCase):
 
     @staticmethod
-    def remove_point_to_be_added(attributes):
+    def remove_int_point_to_be_added(attributes):
         execute_and_commit("DELETE FROM values WHERE point_id = %s AND timestamp = %s AND int = %s",
                            attributes)
 
-    def test_get_add_single(self):
+    @staticmethod
+    def remove_double_point_to_be_added(attributes):
+        execute_and_commit(
+            "DELETE FROM values WHERE point_id = %s AND timestamp = %s AND double = %s",
+            attributes)
+
+    def test_add_get_single(self):
         point_name = ('EV.RM107.RT',)
         timestamp = 23
         value = 8
@@ -17,7 +23,7 @@ class ValueTests(unittest.TestCase):
         start_time = 9
         end_time = 80
 
-        self.remove_point_to_be_added(((3,), start_time, end_time))
+        self.remove_int_point_to_be_added(((3,), start_time, end_time))
 
         Values.add([(point_name, timestamp, value)])
         result = Values.get((3,), start_time, end_time)
@@ -27,11 +33,17 @@ class ValueTests(unittest.TestCase):
                          """[{"value_id":3,"point_name":"EV.RM107.RT","timestamp":13,"value":10}, 
  {"value_id":,"point_name":"EV.RM107.RT","timestamp":23,"value":8}]""")
 
-        self.remove_point_to_be_added(((3,), start_time, end_time))
+        self.remove_int_point_to_be_added(((3,), timestamp, value))
 
     def test_add_enum(self):
         Values.add([('CMC.328.SP', 76, 'OFF')])
-        self.remove_point_to_be_added(((2,), 76, 0))
+        self.remove_int_point_to_be_added(((2,), 76, 0))
+
+    def test_add_many(self):
+        Values.add([('CMC.328.SP', 90, 'ON'), ('EV.RM107.RT', 3, 8), ('EV.RM107.SP', 65, 77.8)])
+        self.remove_int_point_to_be_added(((2,), 90, 1))
+        self.remove_int_point_to_be_added(((3,), 3, 8))
+        self.remove_double_point_to_be_added(((4,), 65, 77.8))
 
     def test_get_single(self):
         self.assertEqual(Values.get((1,), 0, 3),
