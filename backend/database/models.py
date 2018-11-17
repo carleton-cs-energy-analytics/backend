3c94ea63b54d4f7a7a787aae81450c61148ac4de
Alex T. Davis <git@alextdavis.me>
2018-11-16 20:22:03 -0600

    modifications to get read for deployment

--- backend/database/models.py
+++ backend/database/models.py
@@ -2,7 +2,8 @@ import os
 import sys
 import psycopg2
 
-CONN = psycopg2.connect(dbname=os.environ.get('DATABASE_NAME') or 'energy-dev',
+CONN = psycopg2.connect(host=os.environ.get('DATABASE_HOST') or 'localhost',
+                        dbname=os.environ.get('DATABASE_NAME') or 'energy-dev',
                         user=os.environ.get('DATABASE_USER') or '',
                         password=os.environ.get('DATABASE_PASSWORD') or '')
 
