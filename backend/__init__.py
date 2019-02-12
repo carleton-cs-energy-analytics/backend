b459c6714e7ecc77c90f90d96afe908a2f21b95a
Alex T. Davis <git@alextdavis.me>
2019-02-12 12:53:30 -0600

    Added Cross-Origin Response Header for debug mode

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,8 +2,12 @@
 from flask import Flask
 from backend.api.routes import api
 from backend.upload.routes import upload
+from flask_cors import CORS
+import os
 
 application = Flask(__name__)
+if os.environ.get('FLASK_DEBUG'):
+    CORS(application, origin='http://localhost:8080')
 
 application.register_blueprint(api, url_prefix='/api')
 application.register_blueprint(upload, url_prefix='/upload')
