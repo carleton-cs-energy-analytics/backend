cfdfd4b3c528d0b4452c5ef6adf86b4609d799b3
Alex T. Davis <git@alextdavis.me>
2019-02-18 19:37:23 -0600

    Removed seed from consideration in tests

--- test-db-reseed.sh
+++ test-db-reseed.sh
@@ -2,5 +2,4 @@
 
 psql energy-test -f ./migrations/drop.sql && \
 psql energy-test -f ./migrations/schema.sql && \
-psql energy-test -f ./migrations/seed.sql && \
 psql energy-test -f ./migrations/dummy_data.sql
