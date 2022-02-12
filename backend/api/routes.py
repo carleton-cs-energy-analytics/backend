13c8ff017214d0a0a8c950a18e6155b6416d8333
Dominic Enriquez <39425629+enriquezd@users.noreply.github.com>
2022-02-11 20:13:18 -0600

    accidentally switched two positional arguments in get_anomalous_values

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -171,7 +171,7 @@ def get_values():
     return Values.get(tuple(point_ids), start_time, end_time, search_sql) or "[]"
 
 @api.route('/anomalies/vent-and-temp', methods=['GET', 'POST'])
-def get_recent_values():
+def get_anomalous_values():
     start_time = request.values.get('start_time')
     end_time = request.values.get('end_time')
     vent = request.values.get('vent')
@@ -180,7 +180,7 @@ def get_recent_values():
     if None in (start_time, end_time, vent, temp):
         abort(400, "Missing required parameter")
     
-    return Values.temp_vent_anomolies(start_time, end_time, vent, temp)
+    return Values.temp_vent_anomolies(start_time, end_time, temp, vent)
 
 
 @api.route('/values/add', methods=['POST'])
